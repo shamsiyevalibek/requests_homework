edf0b0ca2cee3a928ee8740bb1ec673b5fc0a773
unknown <shamsiyevalibek90@gmail.com>
2025-05-15 21:08:47 +0300

    samir58

--- task3/task3.py
+++ task3/task3.py
@@ -22,3 +22,18 @@ Your implementation below:
 """
 
 # Your implementation here
+import requests
+
+def create_post(title, body, user_id):
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    data = {
+        "title": title,
+        "body": body,
+        "userId": user_id
+    }
+
+    response = requests.post(url, json=data) 
+
+    return response.json() 
+print(create_post("My Homework", "I am learning requests!", 1))
